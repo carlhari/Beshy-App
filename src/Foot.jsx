cf4208d1ab9dfe6591e1ec814be6e5e51a1bfee2
Lemon-tech24 <alisangcocarl@gmail.com>
2023-07-06 09:38:27 +0800

    Footer added, App Done

---
+++ src/Foot.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Foot() {
+  return (
+   <>
+    <div className='Foot'>
+        @ 2023 | Carl Hari
+    </div>
+   </>
+  )
+}
+
+export default Foot
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Foot from './Foot.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Foot />
   </React.StrictMode>,
 )
